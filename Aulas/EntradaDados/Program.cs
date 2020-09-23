c3201219da0ed5721689a4243fcef7cade1a670a
Camila Cunha <camilaldna@yahoo.com.br>
2020-09-23 11:16:53 -0300

    Entrada de Dados Parte 2 - Usar "." para valores double por enquanto

---
+++ Aulas/EntradaDados/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace EntradaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, nomeCompleto;
+            int    qtde, idade;
+            double preco, altura;
+            
+
+            Console.WriteLine("Entre com seu nome completo:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa:");
+            qtde = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("Entre com o seu ultomo nome, idade e altura(mesma linha): ");
+            string[] info = Console.ReadLine().Split(" ");
+            nomeCompleto = info[0];
+            idade = int.Parse(info[1]);
+            altura = double.Parse(info[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(nome);
+            Console.WriteLine(qtde);
+            Console.WriteLine(preco);
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+
+        }
+    }
+}
